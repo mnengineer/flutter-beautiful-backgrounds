c1d0d0d990f9d3c67faa05efaab1742bd8b680c5
mnengineer <masashiii0115@gmail.com>
2023-09-04 19:00:03 +0900

    feat: Weather Animation

--- lib/data/animation_item_data.dart
+++ lib/data/animation_item_data.dart
@@ -1,10 +1,16 @@
 import 'package:flutter_beautiful_backgrounds/models/animation_item.dart';
 import 'package:flutter_beautiful_backgrounds/screens/animations/animation_1.dart';
+import 'package:flutter_beautiful_backgrounds/screens/animations/animation_2.dart';
 
 const animationItemData = [
   AnimationItem(
     title: 'Metaballs',
     subtitle: 'https://pub.dev/packages/metaballs',
     targetWidget: Animation1Screen(),
+  ),
+  AnimationItem(
+    title: 'Weather Animation',
+    subtitle: 'https://pub.dev/packages/weather_animation',
+    targetWidget: Animation2Screen(),
   ),
 ];
---
+++ lib/screens/animations/animation_2.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:weather_animation/weather_animation.dart';
+
+class Animation2Screen extends StatelessWidget {
+  const Animation2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          },
+        ),
+        child: PageView(
+          children: WeatherScene.values.map((e) => e.getWeather()).toList(),
+        ),
+      ),
+    );
+  }
+}
