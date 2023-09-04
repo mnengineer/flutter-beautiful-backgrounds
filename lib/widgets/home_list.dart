26339f7617f795fa4a9d1115a5ecc2cbace88021
mnengineer <masashiii0115@gmail.com>
2023-09-04 18:14:57 +0900

    feat: アプリのベースUI作成

---
+++ lib/widgets/home_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_beautiful_backgrounds/models/animation_item.dart';
+
+class HomeList extends StatelessWidget {
+  const HomeList({Key? key, required this.item}) : super(key: key);
+  final List<AnimationItem> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: item.length,
+      itemBuilder: (ctx, index) => Padding(
+        padding: const EdgeInsets.only(
+          top: 15.0,
+          left: 12.0,
+          right: 12.0,
+        ),
+        child: Material(
+          borderRadius: BorderRadius.circular(12.0),
+          elevation: 4.0,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(12.0),
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => item[index].targetWidget,
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: ListTile(
+                title: Text(
+                  item[index].title,
+                ),
+                subtitle: Text(
+                  item[index].subtitle,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
